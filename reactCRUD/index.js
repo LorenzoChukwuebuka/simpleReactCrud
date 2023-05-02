95c4d71ae89423eb5c4c2f1b189f14b4e948fdde
LorenzoChukwuebuka <Lawrenceobi2@gmail.com>
2023-05-02 11:10:37 +0100

    done

--- reactCRUD/index.js
+++ reactCRUD/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors')
-
-
 const app = express();
-const port = 3000;
-
+const port = 3001;
+ 
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
